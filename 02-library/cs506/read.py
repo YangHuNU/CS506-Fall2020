daaec3fa6b309e08971217d151f56542b8dbdbbc
Yang Hu <s.guyuemuyi@gmail.com>
2020-09-20 21:51:05 -0400

    modified 01-library

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -1,6 +1,16 @@
 def read_csv(csv_file_path):
-    """
-        Given a path to a csv file, return a matrix (list of lists)
-        in row major.
-    """
-    raise NotImplementedError()
+	"""
+		Given a path to a csv file, return a matrix (list of lists)
+		in row major.
+	"""
+	data = []
+	with open(csv_file_path, 'r') as csv_file:
+		lines = csv_file.readlines()
+		for line in lines:
+			row = line.split(",")
+			# convert string to int
+			new_row = []
+			for i in range(len(row)):
+				new_row.append(eval(row[i]))
+			data.append(new_row)
+	return data
--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,13 +1,62 @@
+import math
+
 def euclidean_dist(x, y):
-    raise NotImplementedError()
+	length = 0
+	squared_sum = 0
+
+	if (len(x)>len(y)):	# for length isn't equal
+		length = len(y)
+	else:
+		length = len(x)
+
+	if length==0:	# return when either one list is empty
+		return 0
+	for i in range(length):
+		squared_sum += (x[i]-y[i])**2
+	res = math.sqrt(squared_sum)
+	return res
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+	length = 0
+	abs_sum = 0
+	if (len(x)>len(y)): # for length isn't equal
+		length = len(y)
+	else:
+		length = len(x)
+	if length==0:	#return when either one list is empty
+		return 0
+	for i in range(length):
+		abs_sum += abs(x[i]-y[i])
+	return abs_sum
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+	if len(x)==0 and len(y)==0:
+		return 1
+	a = set(x)
+	b = set(y)
+	intersect = len(a.intersection(b))
+	union = len(a.union(b))
+	return 1-intersect / union	# dissimilarity
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+	if len(x)==0 and len(y)==0:
+		return 0
+	mulpt = 0
+	squared_sum = 0
+	if (len(x)>len(y)): # for length isn't equal
+		length = len(y)
+	else:
+		length = len(x)
+	if length==0:	#return when either one list is empty
+		return 0
+
+	for i in range(length):
+		squared_sum += (x[i]-y[i])**2
+		mulpt += x[i]*y[i]
+
+	if squared_sum==0:	# denomeanator cannot be 0
+		return 1
+	return mulpt/ math.sqrt(squared_sum)
+
 
 # Feel free to add more
